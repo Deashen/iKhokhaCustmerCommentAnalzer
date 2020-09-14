f54bc0c2a5f878687835d029e19e8179160f59d1
deashen <deashen@gmail.com>
2020-09-14 22:03:20 +0200

    Added reader and write to database with Hibernate.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,9 @@
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
@@ -8,17 +11,55 @@ public class Main {
     {
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("ikhokhaPersistence");
 
-        Conditions conditions = new Conditions();
-        conditions.setConditionsDesc("TestJava");
-        conditions.setConditionsLogic("THaty");
+//        Conditions conditions = new Conditions();
+//        conditions.setConditionsDesc("TestJava");
+//        conditions.setConditionsLogic("THaty");
 
         EntityManager entityManager = entityManagerFactory.createEntityManager();
         entityManager.getTransaction().begin();
 
-        entityManager.persist(conditions);
-        entityManager.getTransaction().commit();
+   		File docPath = new File("docs");
+		File[] commentFiles = docPath.listFiles((d, n) -> n.endsWith(".txt"));
 
+		List<String> feedbackList = new ArrayList<>();
+
+		for (File commentFile : commentFiles) {
+			feedbackList.addAll(readLineFromFile(commentFile));
+		}
+
+        for (String items : feedbackList) {
+            Feedback feedbacklineitem = new Feedback();
+            feedbacklineitem.setFeedbackDesc(items);
+            entityManager.persist(feedbacklineitem);
+
+
+        }
+        entityManager.getTransaction().commit();
         entityManagerFactory.close();
 
+
+    }
+
+    public static List<String> readLineFromFile(File file )
+    {
+        List<String> lineList = new ArrayList<>();
+        Feedback feedbackItem = new Feedback();
+        String line;
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))){
+
+            while ((line = reader.readLine()) != null)
+//                feedbackItem.setFeedbackDesc(line);
+                lineList.add(line);
+
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("File not found: " + file.getAbsolutePath());
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            System.out.println("IO Error processing file: " + file.getAbsolutePath());
+            e.printStackTrace();
+        }
+        return lineList;
     }
 }
